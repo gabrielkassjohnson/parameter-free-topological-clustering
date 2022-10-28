ca5600becde76a428806ed9d58e5efeb550f5248
gabrielkassjohnson <aza13572468@gmail.com>
2022-10-27 22:28:07 -0400

    refactor

--- pogo.py
+++ pogo.py
@@ -104,7 +104,7 @@ class Pogo:
         #change dtype to avoid error?
         #inverted_normed_distance = inverted_normed_distance.astype(np.complex)
         #and square it to increase the weighting
-        inverted_normed_distance = np.power(inverted_normed_distance,4)
+        inverted_normed_distance = np.power(inverted_normed_distance,3)
         self.inverted_normed_distance_ = inverted_normed_distance
         normed_gaps = np.multiply(gaps, inverted_normed_distance)
         #normed_gaps = normed_gaps.astype(np.float)
